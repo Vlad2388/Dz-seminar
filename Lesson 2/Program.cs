6954df2c77446316a10f3d8662c0d5bbe4eb2ef6
Vlad <www.stanok.ru@gmail.com>
2022-05-06 12:16:59 +0300

    lesson 2

---
+++ Lesson 2/Program.cs
@@ -0,0 +1,59 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+/*
+Console.Write("Input first integer number");
+void ShowNumber()
+{
+    int num = Convert.ToInt32 (Console .ReadLine ());
+    int firstNum = num / 10 % 10;
+
+    Console.Write (num + " -> ");
+
+     if(num < 1000 & num > 99)
+     {
+         Console.WriteLine (firstNum);
+     }
+     else
+     {
+         Console.WriteLine ("число не верное");
+     }
+
+    
+
+}
+ShowNumber();
+*/
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+/*
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите номер дня недели");
+ 
+
+void ShowNumber()
+{
+
+    int num = Convert.ToInt32 (Console .ReadLine ());
+
+    Console.Write (num + " -> ");
+    if(num > 5 & num < 8)
+    {
+      Console.WriteLine ("Выходной ");
+    }
+    else
+    {
+     if(num < 1 || num > 7)
+     
+      Console.WriteLine ("неверный номер дня ");
+     
+     else
+     
+      Console.WriteLine ("Рабочий ");
+     }
+
+}    
+    ShowNumber();*/
